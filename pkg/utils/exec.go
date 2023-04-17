c6ea74128298fef1e66777610cbecdad99c4d67b
Daniel Brondani <daniel.brondani@arm.com>
2023-04-17 13:36:28 +0200

    Avoid suppressing messages from executed processes

--- pkg/utils/exec.go
+++ pkg/utils/exec.go
@@ -18,20 +18,15 @@ type RunnerInterface interface {
 
 type Runner struct {
 	outBytes []byte
-	quite    bool
 }
 
 func (r *Runner) Write(bytes []byte) (n int, err error) {
 	r.outBytes = append(r.outBytes, bytes...)
-	if r.quite {
-		return len(bytes), nil
-	}
 	return log.StandardLogger().Out.Write(bytes)
 }
 
 func (r Runner) ExecuteCommand(program string, quiet bool, args ...string) (string, error) {
 	r.outBytes = nil
-	r.quite = quiet
 	cmd := exec.Command(program, args...)
 	cmd.Stdout = &r
 	cmd.Stderr = log.StandardLogger().Out
