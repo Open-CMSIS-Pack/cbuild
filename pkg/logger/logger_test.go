f1d3935785b4d1e4a54535e87113c5699ace71c9
Sourabh Mehta <sourabh.mehta@arm.com>
2024-09-25 10:55:48 +0200

    Improved test case

--- pkg/logger/logger_test.go
+++ pkg/logger/logger_test.go
@@ -9,8 +9,8 @@ package logger
 import (
 	"bytes"
 	"errors"
-	"fmt"
 	"os/exec"
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -21,24 +21,27 @@ func TestCustomErrorMethod(t *testing.T) {
 	var logOutput bytes.Buffer
 	SetOutput(&logOutput)
 
-	// Simulate an exec.ExitError
-	cmd := exec.Command("cmd", "/C", "exit", "1")
+	//TEST 1: Simulate an exec.ExitError
+	var cmd *exec.Cmd
+	if runtime.GOOS == "windows" {
+		// Windows: using cmd to force exit with non-zero status
+		cmd = exec.Command("cmd", "/C", "exit", "1")
+	} else {
+		// Unix-based (Linux/macOS): using sh to force exit with non-zero status
+		cmd = exec.Command("sh", "-c", "exit 1")
+	}
 	err := cmd.Run()
-
 	// Log the error using the custom Error method
 	if exitErr, ok := err.(*exec.ExitError); ok {
 		Error(exitErr)
 	}
-
-	aa := logOutput.String()
-	fmt.Println(aa)
-	// Assert that custom logic was applied by checking the log output for "exit_code"
-	assert.Contains(t, aa, "info cbuild: exit status 1\n")
+	// Assert that custom logic was applied by checking the log output for "exit status"
+	assert.Contains(t, logOutput.String(), "info cbuild: exit status 1\n")
 
 	// Clear the buffer
 	logOutput.Reset()
 
-	// Test logging a generic error
+	//TEST 2: Test logging a generic error
 	genericErr := errors.New("generic error")
 	Error(genericErr)
 	// Assert that the original logrus Error behavior was called
