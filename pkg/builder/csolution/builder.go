adc30cb586f1fe104ce347921ea9b7b6ed9ed4ff
Daniel Brondani <daniel.brondani@arm.com>
2024-11-18 08:32:58 +0100

    [cbuild] Remove special handling for triggering `executes` steps

--- pkg/builder/csolution/builder.go
+++ pkg/builder/csolution/builder.go
@@ -433,18 +433,6 @@ func (b CSolutionBuilder) buildContexts(selectedContexts []string, projBuilders 
 	if !b.Setup {
 		buildSummary := fmt.Sprintf("Build summary: %d succeeded, %d failed - Time Elapsed: %s", buildPassCnt, buildFailCnt, utils.FormatTime(totalBuildTime))
 		sepLen := len(buildSummary)
-		// if no context is specified, run cmake build target 'all' to trigger 'executes' steps
-		if b.Options.UseCbuild2CMake && len(b.Options.Contexts) == 0 && projBuilders[0].(cbuildidx.CbuildIdxBuilder).HasExecutes() {
-			infoMsg := "Check all CMake targets"
-			sepLen := len(infoMsg)
-			utils.PrintSeparator("-", sepLen)
-			utils.LogStdMsg(infoMsg)
-			builder := projBuilders[0].(cbuildidx.CbuildIdxBuilder)
-			builder.Options.Target = "all"
-			if buildErr := builder.Build(); buildErr != nil {
-				err = buildErr
-			}
-		}
 		utils.PrintSeparator("-", sepLen)
 		utils.LogStdMsg(buildSummary)
 		utils.PrintSeparator("=", sepLen)
